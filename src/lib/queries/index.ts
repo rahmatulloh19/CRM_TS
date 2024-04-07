df64eab485f9ae1a3675051533419cd71efecf0c
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-04-08 00:21:25 +0500

    delete slash

--- src/lib/queries/index.ts
+++ src/lib/queries/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const BASE_URL = "https://ts-backend-j45p.onrender.com/";
+export const BASE_URL = "https://ts-backend-j45p.onrender.com";
 
 export const api = createApi({
   reducerPath: "api",
